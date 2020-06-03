85909d76912a019fd2f417b14a69d27c814c4142
Varun Jewalikar <vjewalik@amazon.com>
2020-06-03 17:34:04 +0100

    Add Apache-2.0 license

--- src/test/kotlin/com/amazon/awsssmchaosrunner/attacks/SSMAttackTest.kt
+++ src/test/kotlin/com/amazon/awsssmchaosrunner/attacks/SSMAttackTest.kt
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.amazon.awsssmchaosrunner.attacks
 
 import com.amazon.awsssmchaosrunner.attacks.SSMAttack.Companion.getAttack
